f8e318b22f068913f70cb8aa54fb795a4c7d806c
Fedor Gogolev <knsd@knsd.net>
2015-12-24 01:17:03 +0300

    Add boolean type parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
+use std::fmt;
+
 pub trait FromAscii: Sized {
     type Err;
 
     fn from_ascii(s: &[u8]) -> Result<Self, Self::Err>;
+}
+
+/// An error returned when parsing a `bool` from a ascii string fails.
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseBoolError {
+    ParseBoolError
+}
+
+impl fmt::Display for ParseBoolError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        "provided string was not `true` or `false`".fmt(f)
+    }
+}
+
+impl FromAscii for bool {
+    type Err = ParseBoolError;
+
+    #[inline]
+    fn from_ascii(s: &[u8]) -> Result<bool, ParseBoolError> {
+        match s {
+            b"true"  => Ok(true),
+            b"false" => Ok(false),
+            _       => Err(ParseBoolError::ParseBoolError),
+        }
+    }
 }
