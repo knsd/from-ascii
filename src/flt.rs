f0a15b49b98786a6756166f244f2904bd70b23b6
Fedor Gogolev <knsd@knsd.net>
2016-01-06 09:41:30 +0300

    Add ParseFloatError

--- src/flt.rs
+++ src/flt.rs
@@ -1,7 +1,37 @@
-pub use std::num::ParseFloatError;
+use std::num;
+use std::fmt;
+use std::error::{Error};
 use std::str::{from_utf8_unchecked};
 
 use base::{FromAscii};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseFloatError {
+    kind: FloatErrorKind
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum FloatErrorKind {
+    FloatErrorKind
+}
+
+impl Error for ParseFloatError {
+    fn description(&self) -> &str {
+        "failed to parse float"
+    }
+}
+
+impl From<num::ParseFloatError> for ParseFloatError {
+    fn from(t: num::ParseFloatError) -> ParseFloatError {
+        ParseFloatError { kind: FloatErrorKind::FloatErrorKind }
+    }
+}
+
+impl fmt::Display for ParseFloatError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.description().fmt(f)
+    }
+}
 
 impl FromAscii for f32 {
     type Err = ParseFloatError;
@@ -9,7 +39,7 @@ impl FromAscii for f32 {
     #[inline]
     fn from_ascii(src: &[u8]) -> Result<Self, Self::Err> {
         unsafe {
-            from_utf8_unchecked(src).parse()
+            from_utf8_unchecked(src).parse().map_err(From::from)
         }
     }
 }
@@ -20,7 +50,7 @@ impl FromAscii for f64 {
     #[inline]
     fn from_ascii(src: &[u8]) -> Result<Self, Self::Err> {
         unsafe {
-            from_utf8_unchecked(src).parse()
+            from_utf8_unchecked(src).parse().map_err(From::from)
         }
     }
 }
