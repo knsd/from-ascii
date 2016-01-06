f48ff9cd5ac847b1c97cbaee31d89f2ba1d361cd
Fedor Gogolev <knsd@knsd.net>
2016-01-06 09:50:32 +0300

    Documentation

--- src/base.rs
+++ src/base.rs
@@ -1,3 +1,5 @@
+/// A trait to abstract the idea of creating a new instance of a type from a
+/// ascii string. It's a near clone of standard `FromStr` trait.
 pub trait FromAscii: Sized {
     type Err;
 
