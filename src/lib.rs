53598252bf4e6dd742cd310e4c8914b9af2b42a1
Fedor Gogolev <knsd@knsd.net>
2016-01-07 11:18:32 +0300

    Add module level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//!
+//! This crate provides two traits: `FromAscii` which creates instances from ascii string (`&[u8]`) and `FromAsciiRadix` wich creates only integral values with given radix.
+//!
+//! Usage example:
+//!
+//! ```rust
+//! extern crate from_ascii;
+//!
+//! use from_ascii::{FromAscii, FromAsciiRadix};
+//!
+//!
+//! fn main() {
+//!     println!("{}", f64::from_ascii(b"123.456"));
+//!     println!("{}", i16::from_ascii_radix(b"FF", 16));
+//! }
+//! ```
+
 mod base;
 mod boolean;
 mod num;
